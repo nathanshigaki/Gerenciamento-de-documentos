41b90c24b368a343a6ac54b829491028bc3b5f9f
nathan <nathanshigaki@gmail.com>
2026-07-01 16:00:23 -0400

    Feat(UsuarioService): Implementação do crud

--- src/main/java/com/govmt/sgd/service/UsuarioService.java
+++ src/main/java/com/govmt/sgd/service/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.govmt.sgd.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.govmt.sgd.dto.request.UsuarioRequest;
@@ -29,5 +32,38 @@ public class UsuarioService {
         Usuario usuarioSalvar = usuarioMapper.toUsuarioFromRequest(usuarioRequest);
         Usuario usuarioSalvado = usuarioRepository.save(usuarioSalvar);
         return usuarioMapper.toResponseFromUsuario(usuarioSalvado);
+    }
+
+    public List<UsuarioResponse> getAll(){
+        return usuarioRepository.findAll()
+                .stream()
+                .map(usuarioMapper::toResponseFromUsuario)
+                .toList();
+    }
+
+    public UsuarioResponse findById(UUID id){
+        return usuarioRepository.findById(id)
+                .map(usuarioMapper::toResponseFromUsuario)
+                .orElseThrow(() -> new RuntimeException("N encontrado"));
+    }
+
+    public UsuarioResponse updateUsuario(UsuarioRequest usuarioRequest){
+        UsuarioResponse usuarioResponse = findById(usuarioRequest.id());
+        Usuario usuarioUpdate = usuarioMapper.toUsuarioFromResponse(usuarioResponse);
+
+        if (usuarioResponse.nome() != usuarioRequest.nome()) usuarioUpdate.setNome(usuarioRequest.nome());
+        if (usuarioResponse.email() != usuarioRequest.email()) usuarioUpdate.setEmail(usuarioRequest.email());
+        if (usuarioResponse.senha() != usuarioRequest.senha() && usuarioRequest.senha() != null) usuarioUpdate.setSenha(usuarioRequest.senha());
+        if (usuarioResponse.permissao() != usuarioRequest.permissao()) usuarioUpdate.setPermissao(usuarioRequest.permissao());
+        if (usuarioResponse.atualizadoEm() != usuarioRequest.atualizadoEm()) usuarioUpdate.setAtualizadoEm(usuarioRequest.atualizadoEm());
+
+        Usuario usuarioSalvo = usuarioRepository.save(usuarioUpdate);
+        return usuarioMapper.toResponseFromUsuario(usuarioSalvo);
+    }
+
+    public void deleteUsuario(UUID id){
+        UsuarioResponse usuarioExiste = findById(id);
+        Usuario usuarioApagar = usuarioMapper.toUsuarioFromResponse(usuarioExiste);
+        usuarioRepository.delete(usuarioApagar);
     }
 }
